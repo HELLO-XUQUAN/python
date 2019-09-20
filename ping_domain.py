c7a15df1de103a618c8434b02d20171c176e25ad
HELLO-XUQUAN <55575503+HELLO-XUQUAN@users.noreply.github.com>
2019-09-20 15:02:54 +0800

    Create ping_domain.py

---
+++ ping_domain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import socket
+import time
+import os
+
+import re
+pattern = re.compile(
+    r'^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|'
+    r'([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|'
+    r'([a-zA-Z0-9][-_.a-zA-Z0-9]{0,61}[a-zA-Z0-9]))\.'
+    r'([a-zA-Z]{2,13}|[a-zA-Z0-9-]{2,30}.[a-zA-Z]{2,3})$'
+)
+
+
+def getIP(domain):
+    myaddr = socket.getaddrinfo(domain, 'http')
+    print( domain, myaddr[0][4][0])
+
+if __name__ == '__main__':
+    print("------begin------")
+    with open('ip1.txt','r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if pattern.match(line):  #判断是否符合域名 格式
+                getIP(line)
+            else:
+                print( line ,"not domain！！")
+            
+    print("---ping over!!---")
+    
